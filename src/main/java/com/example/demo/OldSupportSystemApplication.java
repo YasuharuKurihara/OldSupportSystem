98150b9b5bb313343eb2cd0473d71c50f2b575d1
Kurihara <yasuharu.kurihara@enisias.com>
2023-05-26 13:15:23 +0900

    base_code

---
+++ src/main/java/com/example/demo/OldSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldSupportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldSupportSystemApplication.class, args);
+	}
+
+}
