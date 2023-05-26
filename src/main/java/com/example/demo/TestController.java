b5a089293cf01d948b021d99639235c77da2c9d8
D057-MKM30B4\ia <an-niikura@kddi-tech.com>
2023-05-26 14:08:16 +0900

    test commit niikura

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -11,6 +11,7 @@ public class TestController {
 
     @GetMapping(path = "/test")
     public String getTest(HttpServletRequest request) {
+    	//commit test
         return "Hello World!";
     }
 	
