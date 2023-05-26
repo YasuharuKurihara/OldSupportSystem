5acab4fc48fb08984be7169e7780bae0b1ed0507
Kurihara <yasuharu.kurihara@enisias.com>
2023-05-26 16:36:11 +0900

    controller secound commit

--- src/main/java/com/example/demo/MemoController.java
+++ src/main/java/com/example/demo/MemoController.java
@@ -1,18 +1,31 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.EntForm;
+
+@Controller
 public class MemoController {
 	
 	//ホーム画面にアクセスされたとき
 	@RequestMapping("/home")
-	public String sample(Model model) {
+	public String home(Model model) {
 		model.addAttribute("title1", "忘れ物防止メモ帳");
 		model.addAttribute("title2", "体調-超-管理");
 		return "home";
 	}
 	
-	
+	@RequestMapping("/memo/view")
+	public String view(Model model) {
+
+		List<EntForm> list = memodao.searchDb();
+		model.addAttribute("dbList", list);
+		model.addAttribute("title", "一覧ページ");
+		return "view";
+	}
 	
 }
