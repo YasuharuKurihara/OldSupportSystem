85798629211a1379a1761caff6fe25e9e6433ccc
FUSO-TR01 <fusotr001@gmail.com>
2023-05-29 13:06:46 +0900

    commit 4

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,8 @@
 CREATE TABLE form(
-<<<<<<< HEAD
-	time yyyymmdd,
-	comment VARCHAR(100) NOT NULL,,
-=======
+
 	id INT NOT NULL AUTO_INCREMENT,
 	time VARCHAR(100) NOT NULL,
 	memo VARCHAR(100) NOT NULL,
 	PRIMARY KEY(id)
-	
->>>>>>> 26355838ce91790935adedebb10f0e9d86e710e0
+
 );
