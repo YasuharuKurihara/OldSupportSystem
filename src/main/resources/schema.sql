fea1cb1229dfe816f999612911872ec93e4fba6f
Kurihara <yasuharu.kurihara@enisias.com>
2023-05-29 13:37:33 +0900

    commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-CREATE TABLE form(
-=======
 CREATE TABLE memo(
 
-    time Yyyymmdd
->>>>>>> 1729b940d8514ff481bacbb5779df9fb250de55b
 	id INT NOT NULL AUTO_INCREMENT,
+	time VARCHAR(100) NOT NULL,
 	memo VARCHAR(100) NOT NULL,
 	PRIMARY KEY(id)
-<<<<<<< HEAD
 );
-=======
-	);
->>>>>>> 1729b940d8514ff481bacbb5779df9fb250de55b
